1517d9b45757b0132d47892d897d92e5c36d2e10
Zhiyang Ong <ongz@acm.org>
2020-01-16 17:31:33 -0600

    Update build.

--- circuit-simulation/normal-in-memory-comp/logic-gates/plot_circuit_simulation_results.py
+++ circuit-simulation/normal-in-memory-comp/logic-gates/plot_circuit_simulation_results.py
@@ -20,10 +20,14 @@
 
 import matplotlib.pyplot as plt
 from matplotlib.collections import EventCollection
+# Get the set of colors from: matplotlib.colors.TABLEAU_COLORS 
+import matplotlib.colors as mcolors
 import numpy as np
 
 
 """
+	@modified by Zhiyang Ong, January 15, 2020.
+
 	Database list of circuit simulation results.
 	Each entry in the list is a dictionary, where its key is the
 		column header and its value is a list of data associated
@@ -60,10 +64,11 @@ with open(filename, "r") as f_obj:
 						output file for Xyce circuit simulation
 						results.
 				"""
-				temp_column = {current_column_header: []}
+				#temp_column = {current_column_header: []}
 				#simulation_results_database.append(temp_column)
 				#simulation_results_database.update({temp_column:[]})
-				simulation_results_database.setdefault(temp_column, [])
+				#simulation_results_database.setdefault(temp_column, [])
+				simulation_results_database[current_column_header] = []
 		elif "End of Xyce(TM) Simulation" in current_line:
 			print("Finish processing Xyce circuit simulation results.")
 		else:
@@ -78,12 +83,18 @@ with open(filename, "r") as f_obj:
 				# Demonstrate successful enumeration of columns/fields.
 				print("	index=",index,"= associated value=",current_field_of_current_row,"=")
 				print("	index=",index,"= current key-value pair=",current_data_point_of_current_field,"=")
-				print("	index=",index,"= temp_list_column_headers[current_name_of_field]=",temp_list_column_headers[current_name_of_field],"=")
+				print("	index=",index,"= simulation_results_database[current_name_of_field]=",simulation_results_database[current_name_of_field],"=")
+				simulation_results_database[current_name_of_field].append(current_field_of_current_row)
 	print("=	Circuit simulation results:")
-	print(simulation_results_database)
+	#print(simulation_results_database)
+
 
 
 """
+	@modified by Zhiyang Ong, January 15, 2020.
+
+	Ignore the following, since I have my own data to plot.
+	The following generates random data to be plotted in this example.
 # Fixing random state for reproducibility
 np.random.seed(19680801)
 
@@ -101,11 +112,38 @@ xdata2.sort()
 # create some y data points
 ydata1 = xdata1 ** 2
 ydata2 = 1 - xdata2 ** 3
+"""
+
+"""
+	List of named colors.
+
+	References:
+	+ \cite[From Color: List of named colors]{MatplotlibDevelopmentTeam2020a}
+		- : {'tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple', 'tab:brown', 'tab:pink', 'tab:gray', 'tab:olive', 'tab:cyan'}
+
+	Check the number of fields in the Xyce circuit simulation results.
+	print("	size of temp_list_column_headers:",len(temp_list_column_headers),".")
+	print("	size of temp_list_column_headers[1:]:",len(temp_list_column_headers[1:]),".")
+	
+	Print the list of colors in matplotlib.colors.TABLEAU_COLORS.
+	print("	mcolors.TABLEAU_COLORS:",mcolors.TABLEAU_COLORS,".")
+	print("	mcolors.TABLEAU_COLORS[tab:gray]",mcolors.TABLEAU_COLORS['tab:gray'],".")
+"""
+# Ignore coloumn 1, which is the index (i.e., row number),
+if len(temp_list_column_headers[2:]) >= len(mcolors.TABLEAU_COLORS):
+	print("	WARNING!!! There are not enough colors in matplotlib.colors.TABLEAU_COLORS to plot all the ")
+
+
+
 
 # plot the data
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
-ax.plot(xdata1, ydata1, color='tab:blue')
+"""
+	The 2nd column of temp_list_column_headers is always the time
+		stamp of the circuit simulation.
+"""
+ax.plot(temp_list_column_headers[1], ydata1, color='tab:blue')
 ax.plot(xdata2, ydata2, color='tab:orange')
 
 # create the events marking the x data points
@@ -132,4 +170,4 @@ ax.set_title('line plot with data points')
 
 # display the plot
 plt.show()
-"""
+
