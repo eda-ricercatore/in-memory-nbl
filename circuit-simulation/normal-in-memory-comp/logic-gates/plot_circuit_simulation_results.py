5950ee3fcac46d6c959bccba3f2925bcc1d06097
Zhiyang Ong <ongz@acm.org>
2020-01-16 18:04:51 -0600

    Add information about buggy commits and computer and IDE, which caused me to lose an hour's worth of work.

--- circuit-simulation/normal-in-memory-comp/logic-gates/plot_circuit_simulation_results.py
+++ circuit-simulation/normal-in-memory-comp/logic-gates/plot_circuit_simulation_results.py
@@ -10,6 +10,21 @@
 		for a circuit with one input parameter.
 
 
+	IMPORTANT NOTES:
+	+ Computer bugs.
+	January 16, 2020, 18:00 hrs, U.S. CST.
+	Buggy computer did not save my file even though I had saved it,
+		and the GUI of the IDE appeared to have saved it.
+	That said, I noticed the "Terminal" application indicating
+		the last modification time had not changed from 16:00 hrs
+		till about 17:40 hrs.
+	Hence, I lost an hour of work.
+	Also, I noticed that the SSH authentication in the "Terminal"
+		tab for the current working directory lapsed.
+	However, commits to my "gulyas-scripts" repository still worked.
+
+
+
 	References:
 	+ \cite[From Lines, bars and markers: EventCollection Demo]{MatplotlibDevelopmentTeam2020a}
 		- https://matplotlib.org/gallery/lines_bars_and_markers/eventcollection_demo.html;
@@ -17,7 +32,8 @@
 """
 
 
-
+###############################################################
+#	Import packages and modules Python libraries
 import matplotlib.pyplot as plt
 from matplotlib.collections import EventCollection
 # Get the set of colors from: matplotlib.colors.TABLEAU_COLORS 
@@ -39,10 +55,14 @@ simulation_results_database = {}
 temp_list_column_headers = []
 # Path to Xyce circuit
 filename = "inverter_transient.spice.prn"
+# Process the output file of Xyce circuit simulation results.
 with open(filename, "r") as f_obj:
 	simulation_results = f_obj.readlines()
+	# For each line in the output file.
 	for current_line in simulation_results:
+		# Is this line the first line?
 		if "Index       TIME" in current_line:
+			# Yes.
 			print("Found first line of the Xyce circuit simulation results.")
 			temp_list_column_headers = current_line.split()
 			"""
@@ -54,6 +74,10 @@ with open(filename, "r") as f_obj:
 			"""
 			print("	Enumerate columns headers.")
 			for current_column_header in temp_list_column_headers[1:]:
+				"""
+					For each field, associate it with an empty
+						list in a dictionary.
+				"""
 				print("	Current column header is:",current_column_header,".")
 				"""
 					Create a dictionary to store the data associated
@@ -69,8 +93,11 @@ with open(filename, "r") as f_obj:
 				#simulation_results_database.update({temp_column:[]})
 				#simulation_results_database.setdefault(temp_column, [])
 				simulation_results_database[current_column_header] = []
+		# Is this the last line of the output file?
 		elif "End of Xyce(TM) Simulation" in current_line:
+			# Yes.
 			print("Finish processing Xyce circuit simulation results.")
+		# Else, process the next available line in the output file.
 		else:
 			# Current enumerated row of circuit simulation results.
 			temp_row_of_cir_sim_res = current_line.split()
@@ -85,8 +112,6 @@ with open(filename, "r") as f_obj:
 				print("	index=",index,"= current key-value pair=",current_data_point_of_current_field,"=")
 				print("	index=",index,"= simulation_results_database[current_name_of_field]=",simulation_results_database[current_name_of_field],"=")
 				simulation_results_database[current_name_of_field].append(current_field_of_current_row)
-	print("=	Circuit simulation results:")
-	#print(simulation_results_database)
 
 
 
@@ -131,43 +156,32 @@ ydata2 = 1 - xdata2 ** 3
 """
 # Ignore coloumn 1, which is the index (i.e., row number),
 if len(temp_list_column_headers[2:]) >= len(mcolors.TABLEAU_COLORS):
-	print("	WARNING!!! There are not enough colors in matplotlib.colors.TABLEAU_COLORS to plot all the ")
-
-
-
-
-# plot the data
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
-"""
-	The 2nd column of temp_list_column_headers is always the time
-		stamp of the circuit simulation.
-"""
-ax.plot(temp_list_column_headers[1], ydata1, color='tab:blue')
-ax.plot(xdata2, ydata2, color='tab:orange')
-
-# create the events marking the x data points
-xevents1 = EventCollection(xdata1, color='tab:blue', linelength=0.05)
-xevents2 = EventCollection(xdata2, color='tab:orange', linelength=0.05)
-
-# create the events marking the y data points
-yevents1 = EventCollection(ydata1, color='tab:blue', linelength=0.05,
-                           orientation='vertical')
-yevents2 = EventCollection(ydata2, color='tab:orange', linelength=0.05,
-                           orientation='vertical')
-
-# add the events to the axis
-ax.add_collection(xevents1)
-ax.add_collection(xevents2)
-ax.add_collection(yevents1)
-ax.add_collection(yevents2)
-
-# set the limits
-ax.set_xlim([0, 1])
-ax.set_ylim([0, 1])
-
-ax.set_title('line plot with data points')
-
-# display the plot
-plt.show()
+	print("	WARNING!!! There are not enough colors in matplotlib.colors.TABLEAU_COLORS to plot each field with a unique color.")
+else:
+	# plot the data
+	fig = plt.figure()
+	ax = fig.add_subplot(1, 1, 1)
+	"""
+		The 2nd column of temp_list_column_headers is always the time
+			stamp of the circuit simulation.
+	"""
+	ax.plot(temp_list_column_headers[1], ydata1, color='tab:blue')
+	ax.plot(xdata2, ydata2, color='tab:orange')
+	# create the events marking the x data points
+	xevents1 = EventCollection(xdata1, color='tab:blue', linelength=0.05)
+	xevents2 = EventCollection(xdata2, color='tab:orange', linelength=0.05)
+	# create the events marking the y data points
+	yevents1 = EventCollection(ydata1, color='tab:blue', linelength=0.05, orientation='vertical')
+	yevents2 = EventCollection(ydata2, color='tab:orange', linelength=0.05, orientation='vertical')
+	# add the events to the axis
+	ax.add_collection(xevents1)
+	ax.add_collection(xevents2)
+	ax.add_collection(yevents1)
+	ax.add_collection(yevents2)
+	# set the limits
+	ax.set_xlim([0, 1])
+	ax.set_ylim([0, 1])
+	ax.set_title('line plot with data points')
+	# display the plot
+	plt.show()
 
