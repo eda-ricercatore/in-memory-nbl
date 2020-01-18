f2c0abb84c1b8bb2f8e4fb23a68216aa02be032e
Zhiyang Ong <ongz@acm.org>
2020-01-17 23:37:24 -0600

    Update build. Debugging in progress.

--- circuit-simulation/normal-in-memory-comp/logic-gates/plot_circuit_simulation_results.py
+++ circuit-simulation/normal-in-memory-comp/logic-gates/plot_circuit_simulation_results.py
@@ -41,6 +41,43 @@
 """
 
 
+
+
+__author__ = 'Zhiyang Ong'
+__version__ = '1.0'
+__date__ = 'January 17, 2020'
+
+#	The MIT License (MIT)
+
+#	Copyright (c) <2020> <Zhiyang Ong>
+
+#	Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+#	The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+#	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#	Email address: echo "cukj -wb- 23wU4X5M589 TROJANS cqkH wiuz2y 0f Mw Stanford" | awk '{ sub("23wU4X5M589","F.d_c_b. ") sub("Stanford","d0mA1n"); print $5, $2, $8; for (i=1; i<=1; i++) print "6\b"; print $9, $7, $6 }' | sed y/kqcbuHwM62z/gnotrzadqmC/ | tr 'q' ' ' | tr -d [:cntrl:] | tr -d 'ir' | tr y "\n"	Che cosa significa?
+
+
+
+
+
+
+
+
+
+
+
+###############################################################
+#	Import packages and modules from the Python Standard Library.
+import math
+
+
+###############################################################
+#	Import Custom Python Packages and Modules
+
+
 ###############################################################
 #	Import packages and modules Python libraries
 import matplotlib.pyplot as plt
@@ -48,6 +85,27 @@ from matplotlib.collections import EventCollection
 # Get the set of colors from: matplotlib.colors.TABLEAU_COLORS 
 import matplotlib.colors as mcolors
 import numpy as np
+
+
+
+
+
+
+# ============================================================
+##	Method to determine the interval/increment between major ticks
+#		for the x-axis and the y-axis (or y-axes).
+#
+#	@param for_y_axes - Boolean flag to indicate if two
+#				intervals/increments between major ticks need to
+#				be found for the y-axes.
+#				This is used for multiple fields of data, such that
+#					two scales need to be used to plot the data.
+#	@return - .
+#	O(1) method.
+@staticmethod
+def psl_uniform(type_of_signal=bv_signal):
+
+
 
 
 """
@@ -122,9 +180,14 @@ with open(filename, "r") as f_obj:
 				print("	index=",index,"= current key-value pair=",current_data_point_of_current_field,"=")
 				print("	index=",index,"= simulation_results_database[current_name_of_field]=",simulation_results_database[current_name_of_field],"=")
 				"""
-				simulation_results_database[current_name_of_field].append(current_field_of_current_row)
-
-
+				"""
+					Convert number in string data type to float
+						data type before putting it into the database.
+					This avoids data processing of the data as strings,
+						instead of numbers.
+				"""
+				simulation_results_database[current_name_of_field].append(float(current_field_of_current_row))
+# ========================================================
 """
 	@modified by Zhiyang Ong, January 15, 2020.
 
@@ -205,7 +268,7 @@ else:
 	print(":	temp_list_column_headers[1]:",temp_list_column_headers[1],".")
 	print("	len(simulation_results_database[temp_list_column_headers[1]]):",len(simulation_results_database[temp_list_column_headers[1]]),".")
 	#print("	simulation_results_database[temp_list_column_headers[1]]:",simulation_results_database[temp_list_column_headers[1]],".")
-	plots = []
+	data_for_y_axes_for_plotting = []
 	for index, (current_field_name,current_field_data,enumerated_color) in enumerate(zip(temp_list_column_headers[2:],simulation_results_database,mcolors.TABLEAU_COLORS)):
 		print(":	current_field_name:",current_field_name,".")
 		# Name of the field/column.
@@ -218,7 +281,7 @@ else:
 			Plot currently enumerated field (i.e., V_in or V_out)
 				on the y-axis against time column/field on the x-axis.
 		"""
-		plots.append(plt.plot(simulation_results_database[temp_list_column_headers[1]],simulation_results_database[current_field_name], color=enumerated_color))
+		data_for_y_axes_for_plotting.append(plt.plot(simulation_results_database[temp_list_column_headers[1]],simulation_results_database[current_field_name], color=enumerated_color))
 		#ax.plot(simulation_results_database[temp_list_column_headers[1]], simulation_results_database[current_field_name], color=enumerated_color)
 		#xevents1 = EventCollection(simulation_results_database[temp_list_column_headers[1]], color=enumerated_color, linelength=0.05)
 		#yevents1 = EventCollection(simulation_results_database[current_field_name], color=enumerated_color, linelength=0.05, orientation='vertical')
@@ -240,13 +303,33 @@ else:
 	#ax.set_xlim([0, 1])
 	#ax.set_ylim([0, 1])
 	# See https://matplotlib.org/3.1.1/api/_as_gen/matplotlib.pyplot.legend.html
-	plt.legend(tuple(plots), ('V_in(V)', 'Vout(V)'))
+	plt.legend(tuple(data_for_y_axes_for_plotting), ('V_in(V)', 'Vout(V)'))
 	"""
 		See https://matplotlib.org/api/axes_api.html#autoscaling-and-margins
 		and https://matplotlib.org/api/pyplot_api.html
 	"""
-	plt.set_xticks(major_ticks)
-	plt.set_yticks(major_ticks)
+	"""
+		Enumerate all fields/columns concurrently to find the minimum.
+		Assume that the increment for each field/column (other than
+			time) can be unique.
+		See https://matplotlib.org/gallery/subplots_axes_and_figures/two_scales.html.
+			\cite[From Subplots, axes and figures: Plots with different scales]{MatplotlibDevelopmentTeam2020a}
+		See https://matplotlib.org/gallery/subplots_axes_and_figures/fahrenheit_celsius_scales.html
+			\cite[From Subplots, axes and figures: Different scales on the same axes]{MatplotlibDevelopmentTeam2020a}
+	"""
+	number_of_rows = len(simulation_results_database[temp_list_column_headers[1]])
+	index_of_last_row = number_of_rows - 1
+	x_major_increment = math.ceil(index_of_last_row/10)
+	y_min = float(min(simulation_results_database[temp_list_column_headers[1]]))
+	print("	y_min is:",y_min,".")
+	y_max = float(max(simulation_results_database[temp_list_column_headers[1]]))
+	print("	y_max is:",y_max,".")
+	y_major_increment = math.ceil((y_max - y_min)/10)
+	print("	simulation_results_database[temp_list_column_headers[1]]",simulation_results_database[temp_list_column_headers[1]],".")
+	x_major_ticks = range(0, number_of_rows,x_major_increment)
+	y_major_ticks = range(y_min, y_max,y_major_increment)
+	ax.set_xticks(x_major_ticks)
+	ax.set_yticks(y_major_ticks)
 	plt.xlabel('time (s)')
 	plt.ylabel('V_in(V), Vout(V)')
 	plt.title('Plot of Xyce circuit simulation results of V_in(V) and Vout(V) over time (s)')
