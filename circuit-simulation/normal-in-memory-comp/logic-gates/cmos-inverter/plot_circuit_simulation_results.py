45573d2b2c5e000aece10585cf68ac9083417e8c
Zhiyang Ong <ongz@acm.org>
2023-02-13 19:44:18 -0600

    Update script to plot Vin and Vout of inverter.

--- circuit-simulation/normal-in-memory-comp/logic-gates/cmos-inverter/plot_circuit_simulation_results.py
+++ circuit-simulation/normal-in-memory-comp/logic-gates/cmos-inverter/plot_circuit_simulation_results.py
@@ -175,94 +175,7 @@ import string
 """
 df = pd.read_table("./input-files/invert1.cir.csv", delimiter=",")
 print(df)
-with open(simulation_results_filename, "r") as f_obj:
-	simulation_results = f_obj.readlines()
-	# For each line in the output file.
-	for current_line in simulation_results:
-		# Is this line the first line?
-		if "Index       TIME" in current_line:
-			# Yes.
-			print("Found first line of the Xyce circuit simulation results.")
-			temp_list_column_headers = current_line.split()
-			"""
-				Enumerate the list of important columns.
-				Ignore the "Index"/first column, and create a
-					dictionary list for each column.
-				That is, each column would be associated with a
-					list ("value") by its column header ("key").
-			"""
-			print("	Enumerate columns headers.")
-			for current_column_header in temp_list_column_headers[1:]:
-				"""
-					For each field, associate it with an empty
-						list in a dictionary.
-				"""
-				print("	Current column header is:",current_column_header,".")
-				"""
-					Create a dictionary to store the data associated
-						with this column.
-					The key is the column header.
-					The value is an intialized empty list that will
-						store the data from that column in the
-						output file for Xyce circuit simulation
-						results.
-				"""
-				#temp_column = {current_column_header: []}
-				#simulation_results_database.append(temp_column)
-				#simulation_results_database.update({temp_column:[]})
-				#simulation_results_database.setdefault(temp_column, [])
-				simulation_results_database[current_column_header] = []
-		# Is this the last line of the output file?
-		elif "End of Xyce(TM) Simulation" in current_line:
-			# Yes.
-			print("Finish processing Xyce circuit simulation results.")
-		# Else, process the next available line in the output file.
-		else:
-			# Current enumerated row of circuit simulation results.
-			temp_row_of_cir_sim_res = current_line.split()
-			"""
-				Store the data for each column in this row into
-					the appropriate dictionary in the database list
-					of circuit simulation results.
-			"""
-			for index, (current_field_of_current_row,current_data_point_of_current_field,current_name_of_field) in enumerate(zip(temp_row_of_cir_sim_res[1:],simulation_results_database,temp_list_column_headers[1:])):
-				# Demonstrate successful enumeration of columns/fields.
-				"""
-				print("	index=",index,"= associated value=",current_field_of_current_row,"=")
-				print("	index=",index,"= current key-value pair=",current_data_point_of_current_field,"=")
-				print("	index=",index,"= simulation_results_database[current_name_of_field]=",simulation_results_database[current_name_of_field],"=")
-				"""
-				"""
-					Convert number in string data type to float
-						data type before putting it into the database.
-					This avoids data processing of the data as strings,
-						instead of numbers.
-				"""
-				simulation_results_database[current_name_of_field].append(float(current_field_of_current_row))
-# ========================================================
-"""
-	@modified by Zhiyang Ong, January 15, 2020.
 
-	Ignore the following, since I have my own data to plot.
-	The following generates random data to be plotted in this example.
-# Fixing random state for reproducibility
-np.random.seed(19680801)
-
-# create random data
-xdata = np.random.random([2, 10])
-
-# split the data into two parts
-xdata1 = xdata[0, :]
-xdata2 = xdata[1, :]
-
-# sort the data so it makes clean curves
-xdata1.sort()
-xdata2.sort()
-
-# create some y data points
-ydata1 = xdata1 ** 2
-ydata2 = 1 - xdata2 ** 3
-"""
 
 """
 	List of named colors.
@@ -280,131 +193,59 @@ ydata2 = 1 - xdata2 ** 3
 	print("	mcolors.TABLEAU_COLORS[tab:gray]",mcolors.TABLEAU_COLORS['tab:gray'],".")
 """
 # Ignore coloumn 1, which is the index (i.e., row number),
-if len(temp_list_column_headers[2:]) >= len(mcolors.TABLEAU_COLORS):
-	print("	WARNING!!! There are not enough colors in matplotlib.colors.TABLEAU_COLORS to plot each field with a unique color.")
-else:
-	# plot the data
-	fig = plt.figure()
-	ax = fig.add_subplot(1, 1, 1)
-	"""
-		The 1st column of temp_list_column_headers is always the row
-			index of the Xyce circuit simulation results.
-		The 2nd column of temp_list_column_headers is always the time
-		stamp of the circuit simulation.
-		Hence, enumerate the fields/columns of the Xyce circuit
-			simulation results from column 2.
-		Column 2 forms the data set for the x-axis, and the other
-			columns are plotted in the x-y plane for the corresponding
-			value of x (i.e., value of time instance).
-		
-		Create the events marking the x data points.
-		Create the events marking the y data points.
-		Add the events to the axis.
+#if len(temp_list_column_headers[2:]) >= len(mcolors.TABLEAU_COLORS):
+#	print("	WARNING!!! There are not enough colors in matplotlib.colors.TABLEAU_COLORS to plot each field with a unique color.")
 
-		ax.plot(temp_list_column_headers[1], ydata1, color='tab:blue')
-		xevents1 = EventCollection(xdata1, color='tab:blue', linelength=0.05)
-		yevents1 = EventCollection(ydata1, color='tab:blue', linelength=0.05, orientation='vertical')
-		ax.add_collection(xevents1)
-		ax.add_collection(yevents1)
-		ax.plot(xdata2, ydata2, color='tab:orange')
-		xevents2 = EventCollection(xdata2, color='tab:orange', linelength=0.05)
-		yevents2 = EventCollection(ydata2, color='tab:orange', linelength=0.05, orientation='vertical')
-		ax.add_collection(xevents2)
-		ax.add_collection(yevents2)
-	"""
-	"""
-	print("	temp_list_column_headers[1]:",temp_list_column_headers[1],".")
-	print("	simulation_results_database[temp_list_column_headers[1]]:",simulation_results_database[temp_list_column_headers[1]],".")
-	print("	temp_list_column_headers[2:]:",temp_list_column_headers[2:],".")
-	"""
-	print(":	temp_list_column_headers[1]:",temp_list_column_headers[1],".")
-	print("	len(simulation_results_database[temp_list_column_headers[1]]):",len(simulation_results_database[temp_list_column_headers[1]]),".")
-	#print("	simulation_results_database[temp_list_column_headers[1]]:",simulation_results_database[temp_list_column_headers[1]],".")
-	data_for_y_axes_for_plotting = []
-	for index, (current_field_name,current_field_data,enumerated_color) in enumerate(zip(temp_list_column_headers[2:],simulation_results_database,mcolors.TABLEAU_COLORS)):
-		print(":	current_field_name:",current_field_name,".")
-		# Name of the field/column.
-		print(":	temp_list_column_headers[index]:",temp_list_column_headers[index+2],".")
-		# Data set for the field/column.
-		print("	len(simulation_results_database[current_field_name]):",len(simulation_results_database[current_field_name]),".")
-		#print(":	simulation_results_database[current_field_name]:",simulation_results_database[current_field_name],".")
-		print("	current color:",enumerated_color,".")
-		"""
-			Plot currently enumerated field (i.e., V_in or V_out)
-				on the y-axis against time column/field on the x-axis.
-		"""
-		data_for_y_axes_for_plotting.append(plt.plot(simulation_results_database[temp_list_column_headers[1]],simulation_results_database[current_field_name], color=enumerated_color))
-		#ax.plot(simulation_results_database[temp_list_column_headers[1]], simulation_results_database[current_field_name], color=enumerated_color)
-		#xevents1 = EventCollection(simulation_results_database[temp_list_column_headers[1]], color=enumerated_color, linelength=0.05)
-		#yevents1 = EventCollection(simulation_results_database[current_field_name], color=enumerated_color, linelength=0.05, orientation='vertical')
-		#ax.add_collection(xevents1)
-		#ax.add_collection(yevents1)
-		"""
-		ax.plot(temp_list_column_headers[1], ydata1, color='tab:blue')
-		xevents1 = EventCollection(xdata1, color='tab:blue', linelength=0.05)
-		yevents1 = EventCollection(ydata1, color='tab:blue', linelength=0.05, orientation='vertical')
-		ax.add_collection(xevents1)
-		ax.add_collection(yevents1)
-		ax.plot(xdata2, ydata2, color='tab:orange')
-		xevents2 = EventCollection(xdata2, color='tab:orange', linelength=0.05)
-		yevents2 = EventCollection(ydata2, color='tab:orange', linelength=0.05, orientation='vertical')
-		ax.add_collection(xevents2)
-		ax.add_collection(yevents2)
-		"""
-	# set the limits
-	#ax.set_xlim([0, 1])
-	#ax.set_ylim([0, 1])
-	# See https://matplotlib.org/3.1.1/api/_as_gen/matplotlib.pyplot.legend.html
-	plt.legend(tuple(data_for_y_axes_for_plotting), ('V_in(V)', 'Vout(V)'))
-	"""
-		See https://matplotlib.org/api/axes_api.html#autoscaling-and-margins
-		and https://matplotlib.org/api/pyplot_api.html
-	"""
-	"""
-		Enumerate all fields/columns concurrently to find the minimum.
-		Assume that the increment for each field/column (other than
-			time) can be unique.
-		See https://matplotlib.org/gallery/subplots_axes_and_figures/two_scales.html.
-			\cite[From Subplots, axes and figures: Plots with different scales]{MatplotlibDevelopmentTeam2020a}
-		See https://matplotlib.org/gallery/subplots_axes_and_figures/fahrenheit_celsius_scales.html
-			\cite[From Subplots, axes and figures: Different scales on the same axes]{MatplotlibDevelopmentTeam2020a}
-	"""
-	"""
-	number_of_rows = len(simulation_results_database[temp_list_column_headers[1]])
-	index_of_last_row = number_of_rows - 1
-	x_major_increment = math.ceil(index_of_last_row/10)
-	y_min = float(min(simulation_results_database[temp_list_column_headers[1]]))
-	print("	y_min is:",y_min,".")
-	y_max = float(max(simulation_results_database[temp_list_column_headers[1]]))
-	print("	y_max is:",y_max,".")
-	y_major_increment = math.ceil((y_max - y_min)/10)
-	print("	simulation_results_database[temp_list_column_headers[1]]",simulation_results_database[temp_list_column_headers[1]],".")
-	x_major_ticks = range(0, number_of_rows,x_major_increment)
-	y_major_ticks = range(y_min, y_max,y_major_increment)
-	"""
-	x_major_ticks, y_major_ticks = determine_interval_between_major_ticks_xy_axes(simulation_results_database,temp_list_column_headers)
-	ax.set_xticks(x_major_ticks)
-	ax.set_yticks(y_major_ticks)
-	plt.xlabel('time (s)')
-	plt.ylabel('V_in(V), Vout(V)')
-	plt.title('Plot of Xyce circuit simulation results of V_in(V) and Vout(V) over time (s)')
-	"""
-		@modified by Zhiyang Ong, January 15, 2020.
-		Save plot in PDF format.
-		This save to PDF command has to appear before the .show()
-			command;
-			else, the PDF fuile would be empty.
-		https://github.com/futurestudio/matplotlib-tutorials/tree/master/export_samples
-		
-		Reference:
-			Norman Peitek, "Matplotlib — Save Plots as File", from
-				{\it Future Studio: Future Studio Tutorials},
-				{Fellner, P{\"{o}}hls, Peitek GbR}, Magdeburg,
-				Saxony-Anhalt, Germany, August 5, 2019.
-				Available at: https://futurestud.io/tutorials/matplotlib-save-plots-as-file;
-					last accessed on January 15, 2019.
-	"""
-	plt.savefig(__file__+".pdf")
-	# display the plot
-	plt.show()
 
+
+
+"""
+	Plot the circuit simulation results [Hunter2016].
+
+	When we plot the input and output signals, Vin and Vout, of the
+		inverter, they will overlap and make it hard to read the
+		graphs/plots/functions separately.
+
+	Consequently, we choose to use subplots to stack the graphs/plots/functions
+		vertically, so that they would be easier to analyze/study/examine
+		[TheMatplotlibDevelopmentTeam2023a, API Reference: matplotlib.pyplot: matplotlib.pyplot.subplots: Date tick labels].
+"""
+#plt.plot(df["\{V(IN)+1.0\}"], df["\{V(VOUT)+1.0\}"], label="sine wave")
+#plt.plot(df["{V(IN)+1.0}"], df["{V(VOUT)+1.0}"], label="inverter $V_{out}$, $V_{in}$")
+#plt.plot(df["TIME"], df["{V(IN)+1.0}"], df["TIME"], df["{V(VOUT)+1.0}"], label="inverter $V_{out}$, $V_{in}$")
+#fig, axs = plt.subplots(2,1)
+fig, axs = plt.subplots(2,1, figsize=(10,10))
+axs[0].plot(df["TIME"], df["{V(IN)+1.0}"], label="inverter $V_{in}$")
+axs[1].plot(df["TIME"], df["{V(VOUT)+1.0}"], label="inverter $V_{out}$")
+#plt.plot(df["V(IN)+1.0"], df["V(VOUT)+1.0"], label="sine wave")
+#plt.plot(df["TIME"], df["V\(IN\)+1.0"], label="sine wave")
+
+
+#plt.plot(df_to_np[:,1], df_to_np[:,2], label="sine wave")
+# Add the title to the plot [Hunter2023].
+#axs[0].title('Plot of $V_{in}$ against time $t$')
+axs[0].set_title('Plot of $V_{in}$ (V) against time $t$ (s)')
+#axs[1].title('Plot of $V_{out}$ against time $t$')
+axs[1].set_title('Plot of $V_{out}$ (V) against time $t$ (s)')
+#fig.title('Plot of $V_{in}$ and $V_{out}$ against time $t$')
+# Add mathematical text to the plot [Hunter2023] [Hunter2023a].
+#axs[0].text(300, 0.65, r'$V_{out}$ = $\neg$ $V_{in}$')
+#axs[1].text(300, 0.65, r'$V_{out}$ = $\neg$ $V_{in}$')
+fig.text(300, 0.65, r'$V_{out}$ = $\neg$ $V_{in}$')
+# Label the y-axis.
+axs[0].set_ylabel('$V_{in}$ (V)')
+axs[1].set_ylabel('$V_{out}$ (V)')
+# Label the x-axis.
+axs[0].set_xlabel('time $t$ (s)')
+axs[1].set_xlabel('time $t$ (s)')
+# Add a legend to the plot [Hunter2023b].
+axs[0].legend(title='Function plotted:')
+axs[1].legend(title='Function plotted:')
+"""
+  Save plot in PDF format [Peitek2019] [Peitek2019a].
+	This "save to PDF" command has to appear before the .show() command;
+		else, the PDF file would be empty.
+"""
+plt.savefig("output-files/"+"cmos-inverter-vout-vin"+".pdf")
+# Show the plot via GUI representation and the file plot.
+plt.show()
